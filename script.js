5a4679799c72dd02e45348a9d1e42a0714779c59
Vanisha Samriddhi <71593673+vanishasamriddhi@users.noreply.github.com>
2021-05-24 13:51:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let noun= prompt("Enter Your Name :");
+// let V4= prompt("Enter a V4 Verb :");
+
+let result =" Hey " + noun + " !. Hope you are doing great. Believe in yourself. Your limitation—it's only your imagination. Dream it. Wish it. Do it.Success doesn’t just find you. You have to go out and get it.The harder you work for something, the greater you’ll feel when you achieve it. Regards, Vanisha";
+alert(result);
